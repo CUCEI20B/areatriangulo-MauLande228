2ee576c72fa48a2a11b75f1cf181da14b8688ed9
Mauricio Landeros <jairo.landeros2885@alumnos.udg.mx>
2020-10-05 20:25:14 -0500

    Prueba01

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    int base, altura;
+    cin >> base;
+    cin >> altura;
 
+    cout << (base*altura)/2 << endl;
+    system("pause");
     return 0;
 }
